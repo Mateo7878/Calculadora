21f76a89788f50cf9cfc59c5376c596ce5653788
José Pablo Mateo Cárdenas <mateocardenaspablo@gmail.com>
2023-01-31 16:47:04 -0600

    Calculadora s/n funcionalidad

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import KeyBoard from './Components/keyBoard'
+import KeyFunctions from './Components/keyBoardFunctions'
+function App() {
+  return(
+    <KeyBoard/>
+  )
+}
+
+export default App
